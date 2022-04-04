b71691dcef5adc4870e1581893959ec38ab9b89b
1287329 <61381370+1287329@users.noreply.github.com>
2022-04-04 12:40:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,11 +4,11 @@
 # File name: diy-part2.sh
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
-sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/feeds/luci/luci-app-turboacc/po/zh-cn/turboacc.po
 #删除部分插件
 rm -rf package/lean/luci-app-netdata
 rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-app-uugamebooster
 rm -rf package/lean/luci-app-usb-printer
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
-sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i 's/"管理权"/"改密码"/g' package/luci/modules/luci-base/po/zh-cn/base.po
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/feeds/luci/luci-app-turboacc/po/zh-cn/turboacc.po
