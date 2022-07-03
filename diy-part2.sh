f4bd9b2100d80f4c6c0fbbea119e6eb482b7a6a5
1287329 <61381370+1287329@users.noreply.github.com>
2022-07-03 09:14:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,9 +9,9 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 #活动连接数
 sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 #删除部分插件
-rm -rf package/lean/luci-app-netdata
-rm -rf package/lean/luci-theme-argon
-rm -rf package/lean/luci-app-uugamebooster
-rm -rf package/lean/luci-app-usb-printer
+#rm -rf package/lean/luci-app-netdata
+#rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-app-uugamebooster
+#rm -rf package/lean/luci-app-usb-printer
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/feeds/luci/luci-app-turboacc/po/zh-cn/turboacc.po
