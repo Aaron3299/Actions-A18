691f347d1f35f94673d2a8959c4b20b2c92d82e8
1287329 <61381370+1287329@users.noreply.github.com>
2022-08-13 13:20:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,15 +10,5 @@ echo 'src-git 1287329 https://github.com/1287329/openwrt-diy' >> feeds.conf.defa
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 #echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
-#liuran001软件源
-#git clone https://github.com/liuran001/openwrt-packages package
 #SSR
 #git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
-#adblock plus
-#git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
-#网络监控中文版
-#git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
-#filebrowser
-#git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
-#adguardhome
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
