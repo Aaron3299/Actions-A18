9c77469c6b85fb41b5192f0098d0bd29e13dcf38
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2022-09-18 02:46:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,8 @@
 #echo 'src-git 1287329 https://github.com/1287329/openwrt-diy' >> feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
-#echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
-#SSR
 git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+#git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/filebrowser package/filebrowser
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-filebrowser package/luci-app-filebrowser
 svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
