e985761f33c2e0e9a8ed03c704a331b66d1fcd61
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2022-09-18 20:13:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,10 +5,10 @@
 #
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 #echo 'src-git 1287329 https://github.com/1287329/openwrt-diy' >> feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 #git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 #svn export https://github.com/kiddin9/openwrt-packages/trunk/filebrowser package/filebrowser
