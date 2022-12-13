374910c5217c3de4fec5d08e9957c50d9facee25
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2022-12-13 13:38:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,9 +5,10 @@
 #
 # File name: diy-part1.sh
 #echo 'src-git 1287329 https://github.com/1287329/openwrt-diy' >> feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+#echo 'src-git https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 #git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
 #git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 #git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
-#git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
