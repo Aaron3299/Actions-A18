cdb5316d901454506ef923db86477e24715efcfb
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-04-04 23:50:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,17 +5,3 @@
 #优先安装 passwall 源
 ./scripts/feeds install -a -f -p passwall_packages
 ./scripts/feeds install -a -f -p passwall_luci
-# 修改openwrt登陆地址,把下面的192.168.9.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
-#修改机器名称
-sed -i 's/Openwrt/A18/g' package/base-files/files/bin/config_generate
-#活动连接数
-sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
-#删除部分插件
-#rm -rf package/lean/luci-app-netdata
-#rm -rf package/lean/luci-theme-argon
-#rm -rf package/lean/luci-app-uugamebooster
-#rm -rf package/lean/luci-app-usb-printer
-# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
-sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/feeds/luci/luci-app-turboacc/po/zh-cn/turboacc.po
-
