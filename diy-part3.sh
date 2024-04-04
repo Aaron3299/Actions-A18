c23297e5bda0aa90c45cd99b9e4ee8dbb0ad0a59
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-04-04 23:52:53 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -9,9 +9,15 @@
 # File name: diy-part3.sh
 # Description: OpenWrt DIY script part 3 (After Install feeds)
 #
+# 修改openwrt登陆地址,把下面的192.168.9.1修改成你想要的就可以了
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+#修改机器名称
+sed -i 's/Openwrt/A18/g' package/base-files/files/bin/config_generate
+#活动连接数
+sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+# 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/feeds/luci/luci-app-turboacc/po/zh-cn/turboacc.po
 
 # 替换golang版本为1.22
 rm -rf feeds/packages/lang/golang
@@ -22,3 +28,9 @@ sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/tt
 
 # design修改proxy链接
 sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
+
+#删除部分插件
+#rm -rf package/lean/luci-app-netdata
+#rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-app-uugamebooster
+#rm -rf package/lean/luci-app-usb-printer
