f7b0875a0066c2b6238795f8664d6c2d90262609
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2024-04-04 23:43:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,10 +7,4 @@
 #echo 'src-git 1287329 https://github.com/1287329/openwrt-diy' >> feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 #echo 'src-git https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
-#git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
-#git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
-#git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-dnsfilter package/diy/luci-app-dnsfilter
-#svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/diy/adguardhome
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/diy/luci-app-adguardhome
-svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-openclash package/diy/luci-app-openclash
+#echo 'src-git custom https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
