a3ffa95691df3c0b4664bd2b8b24e7b4b83b310c
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2025-06-16 18:09:04 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -19,7 +19,7 @@ sed -i 's/Openwrt/A18/g' package/base-files/files/bin/config_generate
 sed -i 's/16384/65536/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
 
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
-#sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/feeds/luci/luci-app-turboacc/po/zh-cn/turboacc.po
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/feeds/luci/luci-app-turboacc/po/zh-cn/turboacc.po
 
 #修改版本信息
 sed -i "s/DISTRIB_DESCRIPTION='*.*'/DISTRIB_DESCRIPTION='A18 (build time: $(date +%Y%m%d))'/g"  package/base-files/files/etc/openwrt_release
@@ -35,7 +35,7 @@ git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/l
 sed -i -r "s#navbar_proxy = 'openclash'#navbar_proxy = 'passwall'#g" feeds/luci/themes/luci-theme-design/luasrc/view/themes/design/header.htm
 
 #删除部分插件
-#rm -rf package/lean/luci-app-netdata
-#rm -rf package/lean/luci-theme-argon
-#rm -rf package/lean/luci-app-uugamebooster
-#rm -rf package/lean/luci-app-usb-printer
+rm -rf package/lean/luci-app-netdata
+rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-app-uugamebooster
+rm -rf package/lean/luci-app-usb-printer
